de61e4cb5138f86961857ad1cef9162830f29146
Hasanabbas Momin <75903359+Hasanabbas-Momin@users.noreply.github.com>
2022-11-25 14:05:24 +0530

    Update unit_test_1.py

--- unit_test_1.py
+++ unit_test_1.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 
 import unittest
-from palindrome import checkPalindrome
+from palindrome import f
 
 
 class TestCase(unittest.TestCase):
     def testCase1(self):
-        self.assertEqual(checkPalindrome("12321"), True)
+        self.assertEqual(f("12321"), True)
 
     def testCase2(self):
-        self.assertEqual(checkPalindrome("1212"), False)
+        self.assertEqual(f("1212"), False)
 
     def testCase3(self):
-        self.assertEqual(checkPalindrome("9"), True)
+        self.assertEqual(f("9"), True)
 
 
 if __name__ == '__main__':
