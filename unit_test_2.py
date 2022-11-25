288082d7961372133b6149e1bbd7921e55f2ad3a
Hasanabbas Momin <75903359+Hasanabbas-Momin@users.noreply.github.com>
2022-11-25 14:05:49 +0530

    Update unit_test_2.py

--- unit_test_2.py
+++ unit_test_2.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 import unittest
-from palindrome import checkPalindrome
+from palindrome import f
 
 
 class TestCase(unittest.TestCase):
     def testCase1(self):
-        self.assertEqual(checkPalindrome("-1"), True)
+        self.assertEqual(f("-1"), True)
 
     def testCase2(self):
-        self.assertEqual(checkPalindrome("2.5"), True)
+        self.assertEqual(f("2.5"), True)
 
 
 if __name__ == '__main__':
